78a39d42aae778cd45c7cc1f7f0a0cf3c5b6a3df
Witheredbanana <147104634+Witheredbanana@users.noreply.github.com>
2024-12-20 07:06:02 +0300

    Add files via upload

---
+++ meals.js
@@ -0,0 +1,79 @@
+// meals.js
+const meals = [
+  {
+    keyword: "gaspacho",
+    name: "Гаспачо",
+    price: 200,
+    category: "soups",
+    count: "350 г",
+    image: "images/menu/gazpacho.jpg",
+  },
+  {
+    keyword: "mushroom_soup",
+    name: "Грибной суп-пюре",
+    price: 190,
+    category: "soups",
+    count: "330 г",
+    image: "images/menu/mushroom_soup.jpg",
+  },
+  {
+    keyword: "norwegian_soup",
+    name: "Норвежский суп",
+    price: 275,
+    category: "soups",
+    count: "350 г",
+    image: "images/menu/norwegian_soup.jpg",
+  },
+
+  // Main Dishes
+  {
+    keyword: "fried potatoes with mushrooms",
+    name: "Жареная картошка с грибами",
+    price: 150,
+    category: "main-dishes",
+    count: "400 г",
+    image: "images/menu/friedpotatoeswithmushrooms1.jpg",
+  },
+  {
+    keyword: "lasagna",
+    name: "Лазанья",
+    price: 300,
+    category: "main-dishes",
+    count: "320 г",
+    image: "images/menu/lasagna.jpg",
+  },
+  {
+    keyword: "chicken_cutlets",
+    name: "Котлеты из курицы с картофельным",
+    price: 350,
+    category: "main-dishes",
+    count: "350 г",
+    image: "images/menu/chickencutletsandmashedpotatoes.jpg",
+  },
+
+  // Drinks
+  {
+    keyword: "orange_juice",
+    name: "Апельсиновый сок",
+    price: 100,
+    category: "drinks",
+    count: "300 мл",
+    image: "images/menu/orangejuice.jpg",
+  },
+  {
+    keyword: "apple_juice",
+    name: "Яблочный сок",
+    price: 90,
+    category: "drinks",
+    count: "300 мл",
+    image: "images/menu/applejuice.jpg",
+  },
+  {
+    keyword: "carrot_juice",
+    name: "Морковный сок",
+    price: 100,
+    category: "drinks",
+    count: "300 мл",
+    image: "images/menu/carrotjuice.jpg",
+  },
+];
